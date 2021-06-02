621822e7c4e48e1986ff469977298d3dc0c2f3c7
Augs Machado <augustomachadoramos@gmail.com>
2021-06-01 22:14:26 -0300

    Interface initial setup

---
+++ PegSolitaire/app/src/main/java/com/augsmachado/pegsolitaire/MainActivity.java
@@ -0,0 +1,25 @@
+package com.augsmachado.pegsolitaire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindViews();
+
+
+    }
+
+    private void bindViews(){
+        // Initialize the components in view
+
+    }
+}
