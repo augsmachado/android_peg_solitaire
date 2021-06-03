e30c06564288eb71aca5fd8dfd90765175ba91cb
Augs Machado <augustomachadoramos@gmail.com>
2021-06-03 16:12:51 -0300

    Add generate random number function

--- PegSolitaire/app/src/main/java/com/augsmachado/pegsolitaire/MainActivity.java
+++ PegSolitaire/app/src/main/java/com/augsmachado/pegsolitaire/MainActivity.java
@@ -4,18 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
 import android.view.View;
-import android.widget.ProgressBar;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+    // Initialize constants
+    public static final int MAX_MILLISECONDS = 1000;
+    public static final int MAX_MINUTES = 2;
+    public static final int MAX_RANDOM = 1000;
+
     // Initialize variables
-    TextView mTimer;
+    private TextView mTimer;
+    private TextView mResults;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +34,19 @@ public class MainActivity extends AppCompatActivity {
         bindViews();
 
         // Initialize timer duration
-        long duration = TimeUnit.MINUTES.toMillis(2);
+        long duration = TimeUnit.MINUTES.toMillis(MAX_MINUTES);
+
+
+        // Generate random number
+        int randomNumber = generateRandomNumber();
+        mResults.setText(String.valueOf(randomNumber));
 
         // Initialize countdown timer
-        new CountDownTimer(duration, 1000) {
+        new CountDownTimer(duration, MAX_MILLISECONDS) {
             @Override
             public void onTick(long millisUntilFinished) {
                 // When tick, convert millisecond to minute and second
-                String sDuration = String.format(Locale.ENGLISH, "%02d : %02d",
+                String sDuration = String.format(Locale.ENGLISH, " %02d:%02d",
                         TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
                         TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
                                 TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)));
@@ -52,8 +67,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     private void bindViews(){
         // Initialize the components in view
         mTimer = findViewById(R.id.timer);
+
+        mResults = findViewById(R.id.results);
+
+
+    }
+
+    private int generateRandomNumber() {
+        return ThreadLocalRandom.current().nextInt(MAX_RANDOM);
     }
 }
