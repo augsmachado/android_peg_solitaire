2f777666c391bae62ddfcee9d85f46d78bfa6632
Augs Machado <augustomachadoramos@gmail.com>
2021-06-03 21:32:43 -0300

    Subtract corresponding clicked value

--- PegSolitaire/app/src/main/java/com/augsmachado/pegsolitaire/MainActivity.java
+++ PegSolitaire/app/src/main/java/com/augsmachado/pegsolitaire/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mFirstButton, mSecondButton, mThirdButton, mFourthButton, mFifthButton;
     private Button mSixthButton, mSeventhButton, mEighthButton, mNinthButton;
+    private Button mStartNewGame;
 
 
 
@@ -40,11 +41,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bindViews();
-
-        // Initialize timer duration
-        long duration = TimeUnit.MINUTES.toMillis(MAX_MINUTES);
-
-
+/*
         // Generate random number
         int randomNumber = generateRandomNumber(MAX_RANDOM);
         if (randomNumber < 2) randomNumber = 2;
@@ -57,7 +54,35 @@ public class MainActivity extends AppCompatActivity {
         int results = randomNumber;
         mResults.setText(String.valueOf(results));
 
+        mResults.setText(String.valueOf(randomNumber));
+        String str = (String) mResults.getText();
+        int results = Integer.parseInt(str);
 
+        mResults.setText(String.valueOf(results));
+*/
+
+
+        // Initialize mStartNewGame button behavior
+        mStartNewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Generate random number
+                int randomNumber = generateRandomNumber(MAX_RANDOM);
+                if (randomNumber < 2) randomNumber = 2;
+
+                // Generate game board
+                int[] board = new int[BOARD];
+                board = generateBoardGame(randomNumber);
+
+                // Calculate results
+                int results = randomNumber;
+                mResults.setText(String.valueOf(results));
+            }
+        });
+
+
+        // Initialize timer duration
+        long duration = TimeUnit.MINUTES.toMillis(MAX_MINUTES);
 
         // Initialize countdown timer
         new CountDownTimer(duration, MAX_MILLISECONDS) {
@@ -83,10 +108,57 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
 
+
+        // Set behavior for each button displayed with numbers
+        // When button clicked, subtract corresponding clicked number
+        mFirstButton.setOnClickListener(v -> {
+            String str = (String) mFirstButton.getText();
+            subtractNumber(str);
+        });
+
+        mSecondButton.setOnClickListener(v -> {
+            String str = (String) mSecondButton.getText();
+            subtractNumber(str);
+        });
+
+        mThirdButton.setOnClickListener(v -> {
+            String str = (String) mThirdButton.getText();
+            subtractNumber(str);
+        });
+
+        mFourthButton.setOnClickListener(v -> {
+            String str = (String) mFourthButton.getText();
+            subtractNumber(str);
+        });
+
+        mFifthButton.setOnClickListener(v -> {
+            String str = (String) mFifthButton.getText();
+            subtractNumber(str);
+        });
+
+        mSixthButton.setOnClickListener(v -> {
+            String str = (String) mSixthButton.getText();
+            subtractNumber(str);
+        });
+
+        mSeventhButton.setOnClickListener(v -> {
+            String str = (String) mSeventhButton.getText();
+            subtractNumber(str);
+        });
+
+        mEighthButton.setOnClickListener(v -> {
+            String str = (String) mEighthButton.getText();
+            subtractNumber(str);
+        });
+
+        mNinthButton.setOnClickListener(v -> {
+            String str = (String) mNinthButton.getText();
+            subtractNumber(str);
+        });
     }
 
 
-    private void bindViews(){
+    private void bindViews() {
         // Initialize the components in view
         mTimer = findViewById(R.id.timer);
 
@@ -101,6 +173,19 @@ public class MainActivity extends AppCompatActivity {
         mSeventhButton = findViewById(R.id.line3Button1);
         mEighthButton = findViewById(R.id.line3Button2);
         mNinthButton = findViewById(R.id.line3Button3);
+
+        mStartNewGame = findViewById(R.id.startNewGame);
+
+        // Initialize the behavior of button
+        mFirstButton.setClickable(true);
+        mSecondButton.setClickable(true);
+        mThirdButton.setClickable(true);
+        mFourthButton.setClickable(true);
+        mFifthButton.setClickable(true);
+        mSixthButton.setClickable(true);
+        mSeventhButton.setClickable(true);
+        mEighthButton.setClickable(true);
+        mNinthButton.setClickable(true);
 
     }
 
@@ -155,8 +240,8 @@ public class MainActivity extends AppCompatActivity {
         int sum = 0;
 
         // Summation of values on array
-        for(int i = 0; i < array.length; i++) {
-            sum = sum + array[i];
+        for (int i : array) {
+            sum = sum + i;
         }
 
         // Difference between summation and random number
@@ -173,8 +258,8 @@ public class MainActivity extends AppCompatActivity {
     // Return the last position is not zero
     private int notZero(int[] array) {
         int notZero = 0;
-        for(int i = 0; i < array.length; i++) {
-            if (array[i] != 0) notZero++;
+        for (int i : array) {
+            if (i != 0) notZero++;
         }
         return notZero;
     }
@@ -206,5 +291,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return array;
+    }
+
+    // When button clicked, subtract corresponding clicked number
+    private void subtractNumber(String str) {
+        int buttonValue = Integer.parseInt(str);
+
+        String sResult = (String) mResults.getText();
+        int results = Integer.parseInt(sResult);
+
+        results = results - buttonValue;
+
+        mResults.setText(String.valueOf(results));
     }
 }
